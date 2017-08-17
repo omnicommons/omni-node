ca08ee4518e83c94672f47bc0b4ef2df450823d2
Matthew Stewart <matthew.stewart.mi@gmail.com>
2017-08-17 15:58:16 -0700

    format

--- app/models/collective.js
+++ app/models/collective.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const collectiveSchema = mongoose.Schema({
-    name: String,
-    description: String,
-    admin: { type: String, ref: "User" }, 
-    members: [{ type: String, ref: "User"}],
-    events: [{ type: String, ref: "Event"}]
+    name        : String,
+    description : String,
+    admin       : { type: String, ref: "User" }, 
+    members     : [{ type: String, ref: "User"}],
+    events      : [{ type: String, ref: "Event"}]
 });
 
 module.exports = mongoose.model('Collective', collectiveSchema);
