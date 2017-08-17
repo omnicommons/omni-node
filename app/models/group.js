385eecc22605b1d3b794a61d2ffeda6a2692af58
Matthew Stewart <matthew.stewart.mi@gmail.com>
2017-08-17 16:30:28 -0700

    add group

---
+++ app/models/group.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const groupSchema = mongoose.Schema({
+    name        : String,
+    description : String,
+    collective  : [{ type: String, ref: "Collective" }],
+    members     : [{ type: String, ref: "User" }]
+});
+
+module.exports = mongoose.model('Group', groupSchema);
