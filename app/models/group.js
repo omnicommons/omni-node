18a44c6a27dc24add2e6a3ab0bad8ac8acfd3268
Matthew Stewart <matthew.stewart.mi@gmail.com>
2017-08-17 16:57:26 -0700

    update attr

--- app/models/group.js
+++ app/models/group.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const groupSchema = mongoose.Schema({
     name        : String,
     description : String,
+    type        : String,
+    building    : { type: String, ref: "Building" },
     collective  : { type: String, ref: "Collective" },
-    members     : [{ type: String, ref: "User" }]
+    members     : [{ type: String, ref: "User" }],
+    events      : [{ type: String, ref: "Event" }]
 });
 
 module.exports = mongoose.model('Group', groupSchema);
